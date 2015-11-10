b35217922b55ad8b9f769f050390b5e0cbbfb063
itrascastro <itrascastro@gmail.com>
2015-11-10 10:37:03 +0100

    Calculator
    First Symfony app using full MVC components

---
+++ src/AppBundle/Controller/CalculatorController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class CalculatorController extends Controller
+{
+    /**
+     * indexAction
+     *
+     * Shows Calculator Menu
+     *
+     * @Route(
+     *     path = "/",
+     *     name = "app_calculator_index"
+     * )
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction()
+    {
+        return $this->render(':calculator:index.html.twig');
+    }
+
+    /**
+     * sumAction
+     *
+     * Shows Calculator Form
+     *
+     * @Route(
+     *     path = "/sum",
+     *     name = "app_calculator_sum"
+     * )
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function sumAction()
+    {
+        return $this->render(':calculator:form.html.twig', ['action' => 'app_calculator_doSum']);
+    }
+
+    /**
+     * doSumAction
+     *
+     * Shows the sum result
+     *
+     * @Route(
+     *     path = "/do-sum",
+     *     name = "app_calculator_doSum"
+     * )
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function doSumAction(Request $request)
+    {
+        $op1 = $request->request->get('op1');
+        $op2 = $request->request->get('op2');
+
+        $calculator = $this->get('calculator');
+        $calculator->setOp1($op1);
+        $calculator->setOp2($op2);
+        $calculator->sum();
+        $result = $calculator->getResult();
+
+        return $this->render(':calculator:result.html.twig', ['result' => $result]);
+    }
+}
