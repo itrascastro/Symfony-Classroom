3487851f2f97807bf7b04c4c093e3af30d81a6f3
itrascastro <itrascastro@gmail.com>
2015-11-28 14:35:46 +0100

    FOSUserBundle installed

---
+++ src/Trascastro/UserBundle/Entity/User.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * (c) Ismael Trascastro <i.trascastro@gmail.com>
+ *
+ * @link        http://www.ismaeltrascastro.com
+ * @copyright   Copyright (c) Ismael Trascastro. (http://www.ismaeltrascastro.com)
+ * @license     MIT License - http://en.wikipedia.org/wiki/MIT_License
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Trascastro\UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="`user`")
+ * @ORM\Entity()
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
