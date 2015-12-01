dacdc6f7d1fc7653208b415bb376bfbe5cc4540d
itrascastro <itrascastro@gmail.com>
2015-12-01 10:46:34 +0100

    using expanded in choice type

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -13,7 +13,7 @@ class UserType extends AbstractType
         $builder
             ->add('email', 'email', ['error_bubbling' => true, 'attr' => ['class' => 'anyClass']])
             ->add('username', 'text', ['error_bubbling' => true])
-            ->add('roles', 'choice', ['multiple' => true, 'choices' => ['ROLE_USER' => 'ROLE_USER', 'ROLE_ADMIN' => 'ROLE_ADMIN']])
+            ->add('roles', 'choice', ['multiple' => true, 'expanded' => true, 'choices' => ['ROLE_USER' => 'ROLE_USER', 'ROLE_ADMIN' => 'ROLE_ADMIN']])
             ->add('plainPassword', 'repeated', array(
                 'type' => 'password',
                 'first_options'  => ['label' => 'Password'],
